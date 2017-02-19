84ef065dc4068ce0ccd949072476682aed2f6b84
Valentyn.Kurlian <valentyn.kurlian@meinfernbus.de>
2017-02-19 19:40:40 +0100

    autocomplete request

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import AutoCompletion from './autocompletion/AutoCompletion';
+import jp from 'jsonp-promise';
+
+const AUTOCOMPLETE_URL = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=%s';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Autocompletion component</h1>
-        <AutoCompletion autocompleteList={["11", "22", "33"]} />
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            autocompleteList: null,
+        };
+        this.onAutocompleteChange = this.onAutocompleteChange.bind(this);
+    }
+
+    onAutocompleteChange(value) {
+        const url = AUTOCOMPLETE_URL.replace('%s', encodeURIComponent(value));
+        jp(url).promise
+            .then((response) => {
+                if (response && response[1]) {
+                    this.setState({autocompleteList: response[1]})
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+          <div>
+            <h1>Autocompletion component</h1>
+            <AutoCompletion
+                autocompleteList={this.state.autocompleteList}
+                onChange={this.onAutocompleteChange}
+            />
+          </div>
+        )
+    }
 }
--- src/components/autocompletion/AutoCompletion.jsx
+++ src/components/autocompletion/AutoCompletion.jsx
@@ -2,8 +2,38 @@ import React, {PropTypes} from 'react';
 import './autocompletion.scss';
 
 export default class AutoCompletion extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isListOpen: false,
+      value: '',
+    };
+    this.onChange = this.onChange.bind(this);
+  }
 
-  getInput(props){
+  // TODO hide on blur
+
+  onChange(event) {
+    // TODO settimeout ?
+    console.log(event.target.value);
+    this.setState({value: event.target.value});
+    const value = event.target.value.trim();
+
+    if (!value) {
+      this.setState({isListOpen: false});
+    } else if (typeof this.props.onChange === 'function'){
+      this.props.onChange(value);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props != nextProps) {
+      const isListOpen = !!(nextProps.autocompleteList && nextProps.autocompleteList.length);
+      this.setState({isListOpen: isListOpen});
+    }
+  }
+
+  getInput(props) {
     const {InputComponent} = this.props;
     if (InputComponent) {
       return <InputComponent {...props} />;
@@ -12,7 +42,7 @@ export default class AutoCompletion extends React.PureComponent {
   }
 
   getDropDown(){
-    if (this.props.autocompleteList) {
+    if (this.state.isListOpen && this.props.autocompleteList && this.props.autocompleteList.length) {
       return (
           <div className="autocomplete__list-container">
             {this.props.autocompleteList.map((text, index) => (
@@ -28,10 +58,14 @@ export default class AutoCompletion extends React.PureComponent {
   }
 
   render() {
+    const customClass = this.props.attr && this.props.attr.className ? this.props.attr.className : '';
     const props = {
       type: 'text',
-      ...this.props,
-      className: `autocomplete__input ${this.props.className ? this.props.className : ''}`
+      ...this.props.attr,
+      className: `autocomplete__input ${customClass}`,
+      onKeyUp: this.onKeyUp,
+      value: this.state.value,
+      onChange: this.onChange,
     }
 
     return (
@@ -45,5 +79,6 @@ export default class AutoCompletion extends React.PureComponent {
 
 AutoCompletion.propTypes = {
   inputComponent: PropTypes.object,
-
+  autocompleteList: PropTypes.array,
+  onChange: PropTypes.func,
 };
